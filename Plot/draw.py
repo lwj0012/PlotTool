fa663313741df52a5eaf1d4f83f09c8ff005b6ec
Wenjie Liu <lwj0012@gmail.com>
2016-03-15 10:04:46 +0800

    first commit

---
+++ Plot/draw.py
@@ -0,0 +1,184 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+from draw_helper import get_color, get_hatch, get_marker, to_percent, to_scf
+from matplotlib.ticker import FuncFormatter, MultipleLocator
+
+
+def make_fig(large):
+    if large:
+        fig, ax = plt.subplots(1, 1, sharex=True, figsize=(18, 3))
+        return fig, ax
+    else:
+        fig, ax = plt.subplots(1, 1, sharex=True, figsize=(8, 4))
+        return fig, ax
+
+
+def output(fig, path):
+    fig.savefig(path, format='pdf', dpi=1000, bbox_inches='tight')
+
+
+def draw_stack(fig, ax, data, legends, xaxis, y_name, **kwargs):
+    benches = len(data[0])
+    bars = len(data)
+    ind = np.arange(start=0, stop=benches*3, step=3)
+    width = 3.0/(bars+2)
+    for i in range(bars):
+        ax.bar(ind+width + width/2.0,
+               data[i],
+               width,
+               color=kwargs.setdefault('color', 'w'),
+               bottom=np.sum(data[:i], axis=0),
+               edgecolor=get_color(i),
+               hatch=get_hatch(i),
+               label=legends[i])
+    ax.set_ylabel(y_name)
+    ax.set_xticks(ind+bars/2.0*width+width)
+    fig.gca().set_ylim(kwargs.setdefault('y_start', 0), kwargs.setdefault('y_end', 1.2))
+    fig.gca().set_xlim(0, benches*3)
+    ax.set_xticklabels(
+        xaxis,
+        rotation=kwargs.setdefault('rotate', 0),
+        fontsize=kwargs.setdefault('xaxis_fs', 9),
+        va=kwargs.setdefault('xaxis_va', 'top'),
+        ha=kwargs.setdefault('xaxis_ha', 'center'))
+    legend = ax.legend(ncol=kwargs.setdefault('lgd_col', '8'),
+                       bbox_to_anchor=(0., 0.99999, 1., .105),
+                       fontsize=kwargs.setdefault('lgd_fs', 9),
+                       loc='upper center')
+    legend.get_frame().set_alpha(kwargs.setdefault('lgd_alf', 0.5))
+    if kwargs.setdefault('use_percent', '0'):
+        formatter = FuncFormatter(to_percent)
+        fig.gca().yaxis.set_major_formatter(formatter)
+
+
+def draw_hist(fig, ax, data, legends, xaxis, y_name, **kwargs):
+    benches = len(data[0])
+    bars = len(data)
+    ind = np.arange(start=0, stop=benches*3, step=3)
+    width = 3.0/(bars+2)
+    for i in range(bars):
+        ax.bar(ind+i*width+width,
+               data[i],
+               width,
+               color=kwargs.setdefault('color', 'w'),
+               edgecolor=get_color(i),
+               hatch=get_hatch(i),
+               linewidth=1,
+               label=legends[i])
+    ax.set_ylabel(y_name)
+    ax.set_xticks(ind+bars/2.0*width+width)
+    fig.gca().set_ylim(kwargs.setdefault('y_start', 0), kwargs.setdefault('y_end', 1.2))
+    fig.gca().set_xlim(0, benches*3)
+    ax.set_xticklabels(
+        xaxis,
+        rotation=kwargs.setdefault('rotate', 0),
+        fontsize=kwargs.setdefault('xaxis_fs', 9),
+        va=kwargs.setdefault('xaxis_va', 'top'),
+        ha=kwargs.setdefault('xaxis_ha', 'center'))
+    legend = ax.legend(ncol=kwargs.setdefault('lgd_col', '8'),
+                       bbox_to_anchor=(0., 1.02, 1., .102),
+                       fontsize=kwargs.setdefault('lgd_fs', 9),
+                       loc='upper center')
+    legend.get_frame().set_alpha(kwargs.setdefault('lgd_alf', 0.5))
+    if kwargs.setdefault('use_percent', '0'):
+        formatter = FuncFormatter(to_percent)
+        # Set the formatter
+        fig.gca().yaxis.set_major_formatter(formatter)
+
+
+def draw_hist_err(fig, ax, data, legends, xaxis, y_name, **kwargs):
+    benches = len(data[0])
+    bars = len(data)
+    ind = np.arange(start=0, stop=benches*3, step=3)
+    width = 3.0/(bars+2)
+    for i in range(bars/2):
+        ax.bar(ind+i*width+width,
+               data[i],
+               width,
+               color=kwargs.setdefault('color', 'w'),
+               edgecolor=get_color(i),
+               hatch=get_hatch(i),
+               yerr=data[bars/2+i],
+               label=legends[i])
+    ax.set_ylabel(y_name)
+    ax.set_xticks(ind+bars/2.0*width+width)
+    fig.gca().set_ylim(kwargs.setdefault('y_start', 0), kwargs.setdefault('y_end', 1.2))
+    fig.gca().set_xlim(0, benches*3)
+    ax.set_xticklabels(
+        xaxis,
+        rotation=kwargs.setdefault('rotate', 0),
+        fontsize=kwargs.setdefault('xaxis_fs', 9),
+        va=kwargs.setdefault('xaxis_va', 'top'),
+        ha=kwargs.setdefault('xaxis_ha', 'center'))
+    legend = ax.legend(ncol=kwargs.setdefault('lgd_col', '8'),
+                       bbox_to_anchor=(0., 1.02, 1., .102),
+                       fontsize=kwargs.setdefault('lgd_fs', 9),
+                       loc='upper center')
+    legend.get_frame().set_alpha(kwargs.setdefault('lgd_alf', 0.5))
+    if kwargs.setdefault('use_percent', '0'):
+        formatter = FuncFormatter(to_percent)
+        fig.gca().yaxis.set_major_formatter(formatter)
+
+
+def draw_cdf(fig, ax, data, legends, xaxis, y_name, **kwargs):
+    lines = len(data)
+    n_bins = 10000
+    for i in range(lines):
+        ax.hist(data[i],
+                n_bins,
+                normed=1,
+                histtype='step',
+                cumulative=True,
+                edgecolor=get_color(i),
+                linewidth=kwargs.setdefault('lw', 1.5),
+                label=legends[i])
+    ax.set_ylabel(y_name)
+    fig.gca().set_ylim(kwargs.setdefault('y_start', 0), kwargs.setdefault('y_end', 1.2))
+    fig.gca().set_xlim(kwargs.setdefault('x_start', 0), kwargs.setdefault('x_end', np.max(data)))
+    legend = ax.legend(ncol=kwargs.setdefault('lgd_col', '8'),
+                       bbox_to_anchor=(0., 1.00, 1., .101),
+                       fontsize=kwargs.setdefault('lgd_fs', 9),
+                       loc='upper center')
+    legend.get_frame().set_alpha(kwargs.setdefault('lgd_alf', 0.5))
+    if kwargs.setdefault('use_percent', '0'):
+        formatter = FuncFormatter(to_percent)
+        fig.gca().yaxis.set_major_formatter(formatter)
+
+
+def draw_line(fig, ax, data, legends, xaxis, y_name, **kwargs):
+    benches = 0
+    lines = len(data)
+    for i in range(len(data)):
+        if benches < len(data[i]):
+            benches = len(data[i])
+    xticks = np.arange(0, (benches+2), 1)
+    indi = xticks[1:-1] - 0.5
+    for i in range(lines):
+        ax.plot(indi[:len(data[i])],
+                data[i],
+                color=get_color(i),
+                linestyle=kwargs.setdefault('ls', '-'),
+                linewidth=kwargs.setdefault('lw', 1.0),
+                marker=get_marker(i),
+                markersize=kwargs.setdefault('mrk_size', 5),
+                markevery=kwargs.setdefault('mrk_inv', 1),
+                label=legends[i])
+    ax.set_ylabel(y_name)
+    fig.gca().set_xlim(0, benches)
+    fig.gca().set_ylim(kwargs.setdefault('y_start', 0), kwargs.setdefault('y_end', 1.2))
+    ax.set_xticks(indi)
+    ax.set_xticklabels(xaxis,
+                       rotation=kwargs.setdefault('rotate', 0),
+                       fontsize=kwargs.setdefault('xaxis_fs', 9),
+                       va=kwargs.setdefault('xaxis_va', 'top'),
+                       ha=kwargs.setdefault('xaxis_ha', 'center'))
+    legend = ax.legend(ncol=4, bbox_to_anchor=(
+        0., 1.02, 1., .102), fontsize=9, loc='upper center')
+    legend.get_frame().set_alpha(kwargs.setdefault('lgd_alf', 0.5))
+    legend.get_frame().set_zorder(20)
+    if kwargs.setdefault('use_percent', '0'):
+        formatter = FuncFormatter(to_percent)
+        # Set the formatter
+        plt.gca().yaxis.set_major_formatter(formatter)
---
+++ test.py
@@ -0,0 +1,85 @@
+__author__ = 'Wenjie'
+from reader import xlsReader, txtReader
+from draw import make_fig, output, draw_hist, draw_stack, draw_line, draw_cdf
+
+
+datas, legends, xaxiss = xlsReader("/home/liu/Dropbox/ICPP'2016/data-draw.xlsx")
+
+path = "/home/liu/ICPP'2016/"
+
+data = datas['IPC']
+legend = legends['IPC']
+xaxis = xaxiss['IPC']
+fig, ax = make_fig(1)
+para = {
+    'y_start' : 0,
+    'y_end': 1.2
+}
+draw_hist(fig, ax, data, legend, xaxis, 'Normalized IPC', **para)
+output(fig, path + 'IPC.pdf')
+
+data = datas['Energy']
+legend = legends['Energy']
+xaxis = xaxiss['Energy']
+fig, ax = make_fig(1)
+para = {
+    'y_start': 0,
+    'y_end': 1
+}
+draw_hist(fig, ax, data, legend, xaxis, 'Normalized Energy', **para)
+output(fig, path + 'Energy.pdf')
+
+data = datas['Access_latency']
+legend = legends['Access_latency']
+xaxis = xaxiss['Access_latency']
+fig, ax = make_fig(1)
+para = {
+    'y_start' : 0,
+    'y_end': 1.1
+}
+draw_hist(fig, ax, data, legend, xaxis, 'Normalized Access Latency', **para)
+output(fig, path + 'Access_latency.pdf')
+
+data = datas['Queue_latency']
+legend = legends['Queue_latency']
+xaxis = xaxiss['Queue_latency']
+fig, ax = make_fig(1)
+para = {
+    'y_start': 0,
+    'y_end': 1.22
+}
+draw_hist(fig, ax, data, legend, xaxis, 'Normalized Queue Latency', **para)
+output(fig, path + 'Queue_latency.pdf')
+
+data = datas['near_far']
+legend = legends['near_far']
+xaxis = xaxiss['near_far']
+fig, ax = make_fig(0)
+para = {
+    'y_start': 0,
+    'y_end': 1,
+    'use_percent': 1
+}
+draw_stack(fig, ax, data, legend, xaxis, '% of near or far request', **para)
+output(fig, path + 'near_far.pdf')
+
+data = datas['PFP']
+legend = legends['PFP']
+xaxis = xaxiss['PFP']
+fig, ax = make_fig(0)
+para = {
+    'y_start': 1.18,
+    'y_end': 1.36
+}
+draw_line(fig, ax, data, legend, xaxis, 'Average PFP slowdown', **para)
+output(fig, path + 'PFP.pdf')
+
+
+data, legend, xaxiss = txtReader("/home/liu/Macos2014-cbr-rewrite-info")
+
+path = "/home/liu/"
+
+fig, ax = make_fig(0)
+# draw_cdf(fig, ax, data, legend, xaxiss, 'Average PFP slowdown', {'y_start':0, 'y_end':1, 'use_percent':1})
+draw_cdf(fig, ax, data, legend, xaxiss, 'Average PFP slowdown', y_start=0, y_end=1, use_percent=1, lw=1, x_star=0, x_end=250)
+output(fig, path + 'cdf.pdf')
